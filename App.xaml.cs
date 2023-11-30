298674a298b0df3d9b87df9771112796232d2c5f
DELIA-ANAMARIA GAFTONE <delia.gaftone@stud.ubbcluj.ro>
2023-11-30 19:11:15 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace Gaftone_Delia_Lab7
+﻿using System;
+using Gaftone_Delia_Lab7.Data;
+using System.IO;
+using Gaftone_Delia_Lab7.Data;
+
+namespace Gaftone_Delia_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
